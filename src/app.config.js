02d09da08e48b4dc786e1296d3ec81b94d928fe0
sudeepOct20 <sudeepto.career17@gmail.com>
2022-10-20 05:55:00 +0000

    Team Name Updated

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Your Team Name';
+    team = 'Spartans';
     version = '1.0.0';
 }
 module.exports = AppConfig;
